c8da22c4bd1dc7ce191a3594e6fb5da16323bf85
zoilamorales <132085265+zoilamorales@users.noreply.github.com>
2025-01-10 20:44:20 -0400

    Add files via upload

---
+++ index_resources/js/ardoraSopaCFG.js
@@ -0,0 +1,39 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=0;
+var score=0; scoreMax=5; scoreInc=5; scoreDec=0
+var successes=0; successesMax=1; attempts=1; attemptsMax=1;
+var tiTime=false;
+var tiTimeType=1;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var startTime; tiSuccesses=false; tiAttempts=false; tiScore=false;
+typeGame=1;
+var colorBack="#FFFFFF"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="Felicidades!"; messageTime=""; isShowMessage=false;
+var urlOk=""; urlTime="";
+var goURLOk="_blank"; goURLTime="_blank"; 
+var borderOk="#008000"; borderTime="#FF0000"; borderAttempts="#FF0000";borderError="#FF0000";
+var imaSel=0; texSel=0;
+var board=[]; var xAnswer=5; answer=[]; rows=6; cols=10; xKey=-1; yKey=-1; animationX=[]; animationY=[]; animationPosX=[]; animationPosY=[]; animationLetter=[];
+var words=["VEVBTVM=","Wk9PTQ==","TUVFVA==","RURVQ0FDScOTTg==","RElHSVRBTA==","Q09OU1RSVUNUSVZJU01P","Q09ORUNUSVZJU01P","Q09MQUJPUkFUSVZP","QUNUSVZP"];
+var c=[5,4,4,9,7,15,12,12,6];
+var defs=["Permite crear equipos y reuniones virtuales","Su uso es para conferencias de mayor tiempo","Se utiliza para conferencias, empresas, pedagogias","Puede ser formal (escuelas, universidades), no formal (cursos, talleres) e informal (experiencias de vida).","En el contexto educativo, se relaciona con el uso de dispositivos y recursos tecnológicos para facilitar el aprendizaje.
+
+","Es una teoría del aprendizaje que sostiene que el conocimiento se construye activamente por el aprendiz, basado en sus experiencias previas y la inter","Es una teoría del aprendizaje para la era digital que enfatiza la importancia de las conexiones entre personas, recursos e información en redes. Resal","Promueve la interacción y la construcción colectiva de conocimiento.","Describe un enfoque en el que el aprendiz es el principal agente de su proceso educativo, participando de manera dinámica e interactiva en actividades"];
+var wDefs=350; posCheck=[];
+var answers=[]; letters=[]; indexLetters=0; xSel=-1; ySel=-1; iLetter=-1; jLetter=-1;
+var wordsGame="aW5kZXg="; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var numCols=15; numRows=15; sizeCell=20;
+var direction=["HD","HI","VD","VI","X1D","X1I","X2D","X2I"];
+var boardGame=[[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."],[".",".",".",".",".",".",".",".",".",".",".",".",".",".","."]];
+var solved1X=[];solved1Y=[];solved2X=[];solved2Y=[];paintSolved=["N","N","N","N","N","N","N","N","N"];x1=[];y1=[];x2=[];y2=[];
+var fillLetter="ABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚÜ";
